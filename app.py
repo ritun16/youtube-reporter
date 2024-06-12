d9e4abb5606837866415c70c40d0ba866b04b232
sourajit roychowdhury <44939374+ritun16@users.noreply.github.com>
2024-06-12 16:24:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,43 +13,78 @@ st.set_page_config(
 )
 
 st.title("YouTube Video Reporter")
-if "streamed_text" not in st.session_state:
-    st.session_state.streamed_text = ""
+
+if "streamed_summary_text" not in st.session_state:
+    st.session_state.streamed_summary_text = ""
+    
+if "streamed_topic_text" not in st.session_state:
+    st.session_state.streamed_topic_text = ""
 
 with st.sidebar:
     openai_api_key = st.text_input("OpenAI API Key", type="password")
     youtube_link = st.text_input("YouTube Link", value=cfg.SAMPLE_YT_VIDEO_LINK)
     generate_report = st.button("Generate Report")
     
-markdown_output = st.empty()
+# markdown_output = st.empty()
+summary_col, topic_col = st.columns(2)
+summary_markdown_output = summary_col.empty()
+topic_markdown_output = topic_col.empty()
 
 
-async def main():
+async def summary_report():
     file_name = './temp_data/{}.mp3'.format(uuid.uuid4().hex)    
     yt_reporter = YouTubeReporter(openai_api_key, youtube_link, file_name)
 
     status, stream_response, meta_details = await yt_reporter.get_report()
-    #streamed_text = ""
     if status == "SUCCESS":
-        st.session_state.streamed_text = st.session_state.streamed_text + meta_details
+        st.session_state.streamed_summary_text = st.session_state.streamed_summary_text + meta_details
         async for chunk in stream_response:
             chunk_content = chunk.choices[0].delta.content
             if chunk_content is not None:
-                #streamed_text = streamed_text + chunk_content
-                st.session_state.streamed_text = st.session_state.streamed_text + chunk_content
-                #print(chunk_content, end="")
+                st.session_state.streamed_summary_text = st.session_state.streamed_summary_text + chunk_content
                 await asyncio.sleep(0.05)
-                markdown_output.markdown(st.session_state.streamed_text)
+                summary_markdown_output.markdown(st.session_state.streamed_summary_text)
         st.download_button(
-                label="Download Report",
-                data=st.session_state.streamed_text,
-                file_name="report.md")
-        st.session_state.streamed_text = ""
+                label="Download Summary Report",
+                data=st.session_state.streamed_summary_text,
+                file_name="summary_report.md")
+        st.session_state.streamed_summary_text = ""
     else:
         st.error(stream_response)
         print(status, stream_response)
-        st.session_state.streamed_text = ""
+        st.session_state.streamed_summary_text = ""
+        
+async def topic_report():
+    file_name = './temp_data/{}.mp3'.format(uuid.uuid4().hex)    
+    yt_reporter = YouTubeReporter(openai_api_key, youtube_link, file_name)
 
+    status, topic_streamed_responses, _ = await yt_reporter.topic_reports()
+    if status == "SUCCESS":
+        for stream_response in topic_streamed_responses:
+            async for chunk in stream_response:
+                chunk_content = chunk.choices[0].delta.content
+                if chunk_content is not None:
+                    st.session_state.streamed_topic_text = st.session_state.streamed_topic_text + chunk_content
+                    await asyncio.sleep(0.05)
+                    topic_markdown_output.markdown(st.session_state.streamed_topic_text)
+            st.session_state.streamed_topic_text = st.session_state.streamed_topic_text + "\n\n"
+        st.download_button(
+                label="Download Topic Report",
+                data=st.session_state.streamed_topic_text,
+                file_name="topic_report.md")
+        st.session_state.streamed_topic_text = ""
+    else:
+        st.error(topic_streamed_responses)
+        print(status, topic_streamed_responses)
+        st.session_state.streamed_topic_text = ""
+        
+
+async def main():
+    await asyncio.gather(
+        summary_report(),
+        topic_report()
+    )
+    
 if generate_report:
     if openai_api_key.strip() == "" or youtube_link.strip() == "":
         st.warning("Please enter both OpenAI API Key and YouTube Link")
